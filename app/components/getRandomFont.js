442973feb16a744484c6ea0c6dba523a65e0d10f
oMalakaki <james.alexander.canfield@gmail.com>
2023-08-08 18:40:58 -0400

    Initial commit

--- app/components/getRandomFont.js
+++ app/components/getRandomFont.js
@@ -1,21 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
-import {useWindowSize} from "rooks"
+import { useWindowSize } from "rooks";
 
-const getRandomSize = () => Math.floor(Math.random() * ((innerWidth-400) - 500 + 1)) + 500;
+
+const getRandomSize = () =>
+  Math.floor(Math.random() * (1200 - 250 + 1 ) + 250);
+
 const getRandomColor = () => {
-  var colors = ['#b30536', '#229b63', '#12918b', '#f25d0a', '#380d54'];
+  var colors = ["#0e0502", "#229b63", "#12918b", "#f25d0a", "#380d54"];
   let color = "#";
   color = colors[Math.floor(Math.random() * colors.length)];
-  
+
   return color;
 };
 
 const RandomSquare = () => {
-  const { innerWidth, innerHeight} = useWindowSize();
+  const { innerWidth, innerHeight } = useWindowSize();
+
   const [objectSize] = useState(getRandomSize());
 
-  
   const [position, setPosition] = useState({
     x: Math.random() * (innerWidth - objectSize),
     y: Math.random() * (innerHeight - objectSize),
@@ -35,18 +38,17 @@ const RandomSquare = () => {
     };
 
     const moveSquare = () => {
-      
       const newPosition = {
         x: position.x + direction.x * 1, // Adjust speed as needed
         y: position.y + direction.y * 1,
       };
-      
+
       if (
-        newPosition.x < 0- objectSize/2 ||
-        newPosition.x > innerWidth - objectSize/2 ||
-        newPosition.y < 0-objectSize/2 ||
-        newPosition.y > innerHeight - objectSize/2
-     ) {
+        newPosition.x < 0 - objectSize / 2 ||
+        newPosition.x > innerWidth - objectSize / 2 ||
+        newPosition.y < 0 - objectSize / 2 ||
+        newPosition.y > innerHeight - objectSize / 2
+      ) {
         // If hitting the edge, change direction and continue moving
         const newDirection = getRandomDirection();
         setDirection(newDirection);
@@ -54,31 +56,32 @@ const RandomSquare = () => {
         setPosition(newPosition);
       }
     };
-   
-    const handleResize = () => {
 
-      if (position.x > innerWidth - objectSize/2) {
+    const handleResize = () => {
+      if (position.x > innerWidth - objectSize / 2) {
         // If over the boundary, keep inside
-        position.x = innerWidth - position.x + position.x - objectSize/2;
-      } else if (position.y > innerHeight - objectSize/2) {
+        position.x = innerWidth - position.x + position.x - objectSize / 2;
+      } else if (position.y > innerHeight - objectSize / 2) {
         // If over the boundary, keep inside
-        position.y = innerHeight - position.y + position.y - objectSize/2;
-      } else if (position.y > innerHeight - objectSize/2 && position.x > innerWidth - objectSize/2)
-        (position.x = innerWidth - position.x + position.x - objectSize/2),
-          (position.y = innerHeight - position.y + position.y - objectSize/2);
+        position.y = innerHeight - position.y + position.y - objectSize / 2;
+      } else if (
+        position.y > innerHeight - objectSize / 2 &&
+        position.x > innerWidth - objectSize / 2
+      )
+        (position.x = innerWidth - position.x + position.x - objectSize / 2),
+          (position.y = innerHeight - position.y + position.y - objectSize / 2);
     };
     if (typeof window !== "undefined") {
-     window.addEventListener("resize", handleResize);
-    };
-  const interval = setInterval(moveSquare, 2); // Adjust interval as needed
+      window.addEventListener("resize", handleResize);
+    }
+    const interval = setInterval(moveSquare, 2); // Adjust interval as needed
 
     return () => {
       if (typeof window !== "undefined") {
-    window.removeEventListener("resize", handleResize);
-      };
+        window.removeEventListener("resize", handleResize);
+      }
       clearInterval(interval);
-     };
- 
+    };
   }, [position, direction]);
 
   return (
