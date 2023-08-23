60718e4ea7984308e4a95f65dcf9f9bdb09ade5e
oMalakaki <james.alexander.canfield@gmail.com>
2023-08-23 15:22:06 -0400

    optimizing...

--- app/components/resumeModal.js
+++ app/components/resumeModal.js
@@ -1,39 +1,80 @@
 "use client";
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import React, { useState, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
 ).toString();
 
 export default function ResumeModal() {
-    const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    const closeModal = () => {
-        setModalVisible(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
     };
-    
-    const openModal = () => {
-        setModalVisible(true);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
-    
-    return (
-        <>
-        <div id="openModalBtn" onClick={openModal}></div>
-        {modalVisible && (
-            <div className='modal-background'>
-                <div id="closeModalBtn" onClick={closeModal}>
-                    Close
-                </div>
-  
-                <div className="pdf-container">
-                    <object data="/Alex Canfield Resume-9.pdf" type="application/pdf" width="100%" height="100%">
-        <p>Your browser does not support PDFs. <a href="your-pdf-file.pdf">Download the PDF</a> instead.</p>
-      </object>
-                </div>
-            </div>
-        )}
-        </>
-    );
+  }, []);
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  return (
+    <>
+      <div id="openModalBtn" onClick={openModal}>
+        <p>View PDF</p>
+        <Document file="/Alex Canfield Resume-9.pdf">
+          <Page 
+            renderAnnotationLayer={false}
+            renderTextLayer={false}
+            scale={0.3}
+            pageNumber={1}
+          />
+        </Document>
+      </div>
+
+      {modalVisible && (
+        <div className="modal-background">
+          <div id="closeModalBtn" onClick={closeModal} />
+
+          <div className="pdf-container">
+            {windowWidth >= 700 ? (
+              <object
+                data="/Alex Canfield Resume-9.pdf"
+                type="application/pdf"
+                width="100%"
+                height="100%"
+              >
+                <p>
+                  Your browser does not support PDFs.{" "}
+                  <a href="/Alex Canfield Resume-9.pdf">Download the PDF</a> instead.
+                </p>
+              </object>
+            ) : (
+                <Document file="/Alex Canfield Resume-9.pdf">
+                <Page 
+                  renderAnnotationLayer={false}
+                  renderTextLayer={false}
+                  scale={windowWidth / 700}
+                  pageNumber={1}
+                />
+              </Document>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
