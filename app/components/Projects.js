fc2c9a7c8ceb6957742f02e466a33d2c04ff24a7
oMalakaki <james.alexander.canfield@gmail.com>
2023-08-25 13:28:32 -0400

    optimizing...

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -2,81 +2,131 @@ import React, { useState } from "react";
 
 function ProjectDescription(props) {
   return (
-    <div className="">
-      <h1>{props.name}</h1>
-      <p>{props.description}</p>
-    </div>
+    <>
+      <div>
+        <h1>{props.name}</h1>
+        <p>{props.description}</p>
+      </div>
+      <p>Skills Learned: {props.skills}</p>
+    </>
   );
 }
 
 export default function Projects() {
   const organizations = ["SONY", "MAG7", "SYNACK", "GW"];
-  
+
   const organizationData = [
     {
       name: "SONY",
       projects: [
-        { name: "KPI capstone", description: "Description" },
-    
-      ]
+        {
+          name: "KPI capstone",
+          description:
+            "This capstone project spanned the length of my internship with Sony. When I started working, I was presented with a problem, the Program Management Office (PMO) team had risen to a level of maturity where the department wanted to start tracking portfolio metrics and performance over time. However, there was no set way of collecting our performance data or any measurements to compare them to. \nBy collaborating with the PMO team, I played a crucial role in coming up with a step-by-step solution to this problem and then implementing it. I researched industry trends and implemented best practices to develop Key Performance Indicators (KPIs) that aligned with departmental and company goals. Then, I employed my Excel skillset to automate the in-depth analysis of PMO data. This analysis included comprehensible visualizations of each KPI, allowing for actionable insights for the PMO team.",
+          skills:
+            "Excel, KPI Research and Development, Team Collaboration, Data Analysis, Daptiv, Sharepoint",
+        },
+      ],
     },
     {
       name: "MAG7",
       projects: [
-        { name: "mag7 website redesign", description: "Description" },
-
-      ]
+        {
+          name: "mag7 website redesign",
+          description:
+            "For this project, I collaborated with MAG7’s in-house UX/UI designers to build a new website for the company. The website in use was fairly outdated, so a complete design overhaul and a build from scratch were needed. I had the opportunity to sit in on design brainstorms and gain a deeper understanding of the complexities behind UX/UI design and content creation for professional websites. Employing Next.js, I took the designs and translated them into a functional website, integrating the usage of content management systems so the content of the website could be modified and updated with relative ease. ",
+          skills:
+            "Content Management Systems, Next.js, Debugging, UX Design, Figma, APIs, CSS, HTML, JS",
+        },
+      ],
     },
     {
       name: "SYNACK",
       projects: [
-        { name: "So fresh, so clean", description: "Description" },
-
-      ]
+        {
+          name: "So fresh, so clean",
+          description:
+            "While at Synack, I was working in a product team developing an OSINT reporting product for our clients. One of my responsibilities on the job was to extract the data from our OSINT tools, clean it, normalize it, and transform it into usable and presentable information in our reports. This process was long, tedious, and prone to errors. Eventually, I decided there had to be a better way to do this process. By leveraging my technical expertise in Python I had learned from my coursework in school, I innovated and improved upon our product by fully automating the cleaning, normalization, and transformation of data, resulting in fewer errors, and accelerated data analysis and reporting for client reports by hours. This not only saved valuable time but also enabled our team to provide swift and actionable insights to our clients, delivering our clients a clear and digestible security posture report for their organizations.",
+          skills: "Python, Data Cleaning, Data Analysis",
+        },
+      ],
     },
     {
       name: "GW",
       projects: [
-        { name: "Noverint", description: "Description" },
-        { name: "gwsafe", description: "Description" },
-        
-      ]
+        {
+          name: "Noverint",
+          description:
+            "My senior capstone project was to work in a group with other seniors and grad students to put our information system skills to work and design a new digital product, develop a business model for it, and fully design and implement it. My group decided to build an inventory management system aimed at small businesses that were hesitant to move to an existing solution, either due to the complexity, pricing, or inaccessibility of existing products in the current market. Using the Agile methodology we worked together to research the market, develop business assumptions and risks, create and system cost target, and come up with epics, user stories, use cases, data flow diagrams, data dictionaries, screen flows, wireframes, state charts, and more for our product. We then used HTML, CSS, JS, and PHP to fully design and build our MVP for the system.",
+          skills:
+            "Product Research, Design and Development, Product Analysis, Database Design and Development, HTML, CSS, JS, PHP, APIs",
+        },
+        {
+          name: "gwsafe",
+          description:
+            "For this project, I worked with a classmate to develop a functional website for our final project in our web development class. We came up with the idea of redesigning GW’s emergency response website. Our goal was to make a user-friendly, clean website to intelligibly display and report crimes around and near GW’s campus. For the base of our application, we employed Google Map’s API to create an interactive map that displayed crimes in the area. The website also had a reporting feature to report crimes on campus, which had the intention of going through GWPD for confirmation and then displaying on the map for students and faculty to be aware of. Once displayed, users could interact with the posts, with the ability to comment and react to them.",
+          skills: "Web Development, UX/UI Design, APIs, CSS, HTML, JS",
+        },
+      ],
     },
-
   ];
 
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeOrgIndex, setActiveOrgIndex] = useState(0);
+  const [activeProjIndex, setActiveProjIndex] = useState(0);
 
-  const handleOrgContainerClick = (index) => {
-    setActiveIndex(index);
+  const handleOrgContainerClick = (orgIndex) => {
+    setActiveOrgIndex(orgIndex);
+    setActiveProjIndex(0);
   };
+
+  const handleNextProject = () => {
+    setActiveProjIndex(
+      (prevIndex) =>
+        (prevIndex + 1) % organizationData[activeOrgIndex].projects.length
+    );
+  };
+
+  const handlePrevProject = () => {
+    setActiveProjIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + organizationData[activeOrgIndex].projects.length) %
+        organizationData[activeOrgIndex].projects.length
+    );
+  };
+
+  const activeOrgProjects = organizationData[activeOrgIndex].projects;
 
   return (
     <div className="projectsText">
-        <div className="projectsHero">
-        {organizationData[activeIndex].projects.map((project, index) => (
-          <ProjectDescription
-            key={index}
-            name={project.name}
-            description={project.description}
-          />
-          
-        ))}
+      <div className="projectsHero">
+        <ProjectDescription
+          name={activeOrgProjects[activeProjIndex].name}
+          description={activeOrgProjects[activeProjIndex].description}
+          skills={activeOrgProjects[activeProjIndex].skills}
+        />
+        {activeOrgProjects.length > 1 && (
+          <div className="arrowButtons">
+            <button onClick={handlePrevProject}>{"<"}</button>
+            <h4>
+              {activeProjIndex + 1} of {activeOrgProjects.length}
+            </h4>
+            <button onClick={handleNextProject}>{">"}</button>
+          </div>
+        )}
       </div>
       <div className="projectsList">
-        {organizations.map((org, index) => (
+        {organizations.map((org, orgIndex) => (
           <div
             key={org}
             className={`orgContainer ${
-              activeIndex === index ? "active" : ""
+              activeOrgIndex === orgIndex ? "active" : ""
             }`}
-            onClick={() => handleOrgContainerClick(index)}
+            onClick={() => handleOrgContainerClick(orgIndex)}
           >
             <h3>{org}</h3>
           </div>
         ))}
       </div>
-      
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 import Cards from "./components/Cards";
 import MakeBox from "./components/MakeBox";
 import { useEffect, useState } from "react";
-import GenerateBackground from "./components/GenerateBackground"; 
-import Projects from "./components/Projects"; 
-
+import GenerateBackground from "./components/GenerateBackground";
+import Projects from "./components/Projects";
+import { Link, Element } from 'react-scroll';
 
 const HomePage = () => {
   const [windowWidth, setWindowWidth] = useState(0);
@@ -16,7 +16,7 @@ const HomePage = () => {
     };
 
     const handleScroll = () => {
-      if(window.scrollY >= 100) {
+      if (window.scrollY >= 100) {
         setWindowPos(100);
       } else {
         setWindowPos(window.scrollY);
@@ -39,13 +39,19 @@ const HomePage = () => {
         <div className="titleText cut-out-text">
           <div className="menu">
             <h2>
-              <a href="#aboutLink">About</a>
+              <Link to="aboutLink" smooth={true} duration={500}>
+                About
+              </Link>
             </h2>
             <h2>
-              <a href="#resumeLink">Resume</a>
+              <Link to="resumeLink" smooth={true} duration={500}>
+                Resume
+              </Link>
             </h2>
             <h2>
-              <a href="#projectsLink">Projects</a>
+              <Link to="projectsLink" smooth={true} duration={500}>
+                Projects
+              </Link>
             </h2>
           </div>
           <h1>
@@ -62,8 +68,8 @@ const HomePage = () => {
             left: "0",
             top: "0",
             overflow: "hidden",
-            
-            filter: `blur(${ windowPos/2+ "px"})`,
+
+            filter: `blur(${windowPos / 2 + "px"})`,
           }}
         >
           <GenerateBackground />
@@ -72,51 +78,66 @@ const HomePage = () => {
         <img src="alexgraffiti.svg"/>
         </div> */}
       </div>
-      <div className="pageContainer about" id="aboutLink">
-    
-      <div className="sectionTitle">    <h2>About</h2><h2 className="titleDupe">About</h2>
-      </div>
+      <div className="pageContainer about" name="aboutLink">
+        <div className="sectionTitle">
+          {" "}
+          <h2>About</h2>
+          <h2 className="titleDupe">About</h2>
+        </div>
 
-            <p>
-              I am a motivated and experienced graduate in Information Systems
-              with a strong focus on project management and product development.
-              Equipped with a blend of technical prowess and business acumen, I
-              am passionate about utilizing technology to fuel innovation and
-              streamline operations while fostering cross-functional teamwork to
-              yield optimal business value.
-              <br />
-              <br />
-              My professional journey includes diverse experiences, spanning
-              nimble startups to a large international company, enabling me to
-              excel in both agile and meticulously structured team dynamics.
-              This adaptability has honed my ability to deliver impactful
-              contributions within rapidly evolving business landscapes, while
-              infusing creativity and pragmatism.
-            </p>
-         
-   
-        
+        <p>
+          I am a motivated and experienced graduate in Information Systems with
+          a strong focus on project management and product development. Equipped
+          with a blend of technical prowess and business acumen, I am passionate
+          about utilizing technology to fuel innovation and streamline
+          operations while fostering cross-functional teamwork to yield optimal
+          business value.
+          <br />
+          <br />
+          My professional journey includes diverse experiences, spanning nimble
+          startups to a large international company, enabling me to excel in
+          both agile and meticulously structured team dynamics. This
+          adaptability has honed my ability to deliver impactful contributions
+          within rapidly evolving business landscapes, while infusing creativity
+          and pragmatism.
+        </p>
       </div>
       <Cards />
-      <div className="pageContainer resume" id="resumeLink">
-    
-      <div className="sectionTitle">    <h2>Resume</h2><h2 className="titleDupe">Resume</h2></div>
-          <div className="resumeText">
-            <MakeBox />
-            
-          </div>
-   
+      <div className="pageContainer resume" name="resumeLink">
+        <div className="sectionTitle">
+          {" "}
+          <h2>Resume</h2>
+          <h2 className="titleDupe">Resume</h2>
+        </div>
+        <div className="resumeText">
+          <MakeBox />
+        </div>
+      </div>
+
+      <div className="pageContainer projects" name="projectsLink">
+        <div className="sectionTitle">
+          {" "}
+          <h2>Projects</h2>
+          <h2 className="titleDupe">Projects</h2>
         </div>
 
-      <div className="pageContainer projects" id="projectsLink">
-    
-      <div className="sectionTitle">    <h2>Projects</h2><h2 className="titleDupe">Projects</h2></div>
-
-          
-          <Projects />
-          
-        </div>
-   
+        <Projects />
+      </div>
+      <div className="pageContainer connect" style={{marginTop: "5rem", height: "50vh"}}>
+       <h1 style={{fontSize: "6rem", mixBlendMode: "difference"}}>
+        LET'S CONNECT!
+       </h1>
+       <p>james.alexander.canfield@gmail.com</p>
+       <p><a
+          href="https://www.linkedin.com/in/jamescanfield"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin.com/in/jamescanfield
+        </a></p>
+        
+      </div>
+     
     </>
   );
 };
